b7a0eb9722c277928676d15178440bd43ec8257c
Onur Yılmaz <106242434+onurrrylmz@users.noreply.github.com>
2024-08-08 01:11:41 +0300

    Hatırlatma

---
+++ src/main/java/com/onurrrylmz/tutorials/JavaTutorials.java
@@ -0,0 +1,60 @@
+package com.onurrrylmz.tutorials;
+
+import java.nio.file.OpenOption;
+import java.util.Arrays;
+
+public class JavaTutorials {
+    public static void main(String[] args) {
+        // Arrays.sort();
+        //this.
+        //super
+        //break continue return
+        //optional
+        //cast
+        //primitive type
+        //wrapper class
+        //Random
+        //Math
+        //%s Sd %f --->printf()
+        //final
+        //static
+        //decoder encoder
+        //instanceof
+        //public private protected
+        //String
+        //StringBuilder
+        //StringTokenizer
+        //StringBuffer
+        //Enum
+        //Multithread
+        //SOLID
+        //for each
+        //KISS
+        //Dont repeat yourself
+        //MONAD
+        //List Set Map
+        //List:LinkedList ArrayList
+        //Generic<>
+        //Iterator
+        //OOP
+        //interface
+        //abstract new
+        //inheritance
+        //UML - + __
+        //class
+        //POJO
+        //bean
+        //Serileştirme
+        //IO
+        //JAR WAR
+        //Dağıtık Programlama
+        //Server
+        //Constructor
+        //Exception
+        //Error
+        //Java 8: stream,map,filter,sort
+        //Lambda Expression
+        //Method Referances
+        //D.P.
+    }
+}
