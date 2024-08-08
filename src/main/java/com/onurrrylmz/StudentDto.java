39b2ec4d2eb40b5408ff4babc8130cb03a163883
Onur Yılmaz <106242434+onurrrylmz@users.noreply.github.com>
2024-08-09 02:14:22 +0300

    @StudentDto

---
+++ src/main/java/com/onurrrylmz/StudentDto.java
@@ -0,0 +1,20 @@
+package com.onurrrylmz;
+
+
+//Data Transfer Object = DTO
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentDto {
+
+    private Long studentId;
+    private String studentName;
+    private String studentSurname;
+}
