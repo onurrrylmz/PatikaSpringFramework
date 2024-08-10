7c402d3db3506904e3ca0d01f9dc6d0032fdf671
Onur Yılmaz <106242434+onurrrylmz@users.noreply.github.com>
2024-08-10 12:59:32 +0300

    Inversion Of Control(-)

---
+++ src/main/java/com/onurrrylmz/iocsiz_disiz/Calisan.java
@@ -0,0 +1,21 @@
+package com.onurrrylmz.iocsiz_disiz;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named(value = "cdisizCalisan")
+@ApplicationScoped
+
+public class Calisan {
+
+    private Patron patron;
+
+    public Calisan(){
+        patron=new Patron();
+    }
+
+    public String getData(String data){
+        return patron.sürüm("17");
+    }
+}
---
+++ src/main/java/com/onurrrylmz/iocsiz_disiz/Patron.java
@@ -0,0 +1,8 @@
+package com.onurrrylmz.iocsiz_disiz;
+
+public class Patron {
+
+    public String sürüm(String data) {
+        return "sürüm " + data;
+    }
+}
