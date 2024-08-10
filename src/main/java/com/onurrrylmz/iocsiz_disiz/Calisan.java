68e5eea77d158b74871d8451cff2e92e714cd6ec
Onur Yılmaz <106242434+onurrrylmz@users.noreply.github.com>
2024-08-10 13:00:30 +0300

    Inversion Of Control(-) Dependency Injection(-)

--- src/main/java/com/onurrrylmz/iocsiz_disiz/Calisan.java
+++ src/main/java/com/onurrrylmz/iocsiz_disiz/Calisan.java
@@ -7,6 +7,7 @@ import javax.inject.Named;
 @Named(value = "cdisizCalisan")
 @ApplicationScoped
 
+
 public class Calisan {
 
     private Patron patron;
--- src/main/java/com/onurrrylmz/iocsiz_disiz/Patron.java
+++ src/main/java/com/onurrrylmz/iocsiz_disiz/Patron.java
@@ -3,6 +3,7 @@ package com.onurrrylmz.iocsiz_disiz;
 public class Patron {
 
     public String sürüm(String data) {
+
         return "sürüm " + data;
     }
 }
